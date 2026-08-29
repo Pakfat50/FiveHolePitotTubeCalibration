8047b9c662cab933308394ddcfe4de7c56b69716
Pakfat50 <66896757+Pakfat50@users.noreply.github.com>
2026-08-29 23:56:29 +0900

    Add UC-06 traceability link

--- docs/hooks.py
+++ docs/hooks.py
@@ -14,7 +14,7 @@ _TRACE_TARGETS = {
         "UC-05（シミュレーションする）",
         "../architecture_design/#uc-05",
     ),
-    "CODE:simulation.SimulationController.start": (
+    "ARCH:UC-06": (\n        "UC-06（Gコードを生成する）",\n        "../architecture_design/#uc-06",\n    ),\n    "CODE:simulation.SimulationController.start": (
         "SimulationController.start",
         "../api/#simulation.SimulationController.start",
     ),
