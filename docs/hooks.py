2883188bd000122e953fb9aa0f41e1d85f15499c
Pakfat50 <66896757+Pakfat50@users.noreply.github.com>
2026-08-29 21:35:17 +0900

    feat: support simulation test trace links

--- docs/hooks.py
+++ docs/hooks.py
@@ -34,6 +34,82 @@ _TRACE_TARGETS = {
         "test_start_sets_playing_state_at_first_point",
         "../test-api/#tests.test_simulation.TestSimulation.test_start_sets_playing_state_at_first_point",
     ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_start_frame_is_first_point": (
+        "test_start_frame_is_first_point",
+        "../test-api/#tests.test_simulation.TestSimulation.test_start_frame_is_first_point",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_end_frame_is_last_point": (
+        "test_end_frame_is_last_point",
+        "../test-api/#tests.test_simulation.TestSimulation.test_end_frame_is_last_point",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_middle_progress_maps_to_scan_order": (
+        "test_middle_progress_maps_to_scan_order",
+        "../test-api/#tests.test_simulation.TestSimulation.test_middle_progress_maps_to_scan_order",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_playback_duration_is_independent_of_hold_time": (
+        "test_playback_duration_is_independent_of_hold_time",
+        "../test-api/#tests.test_simulation.TestSimulation.test_playback_duration_is_independent_of_hold_time",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_side_view_is_initialized": (
+        "test_side_view_is_initialized",
+        "../test-api/#tests.test_simulation.TestSimulation.test_side_view_is_initialized",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_front_view_is_initialized": (
+        "test_front_view_is_initialized",
+        "../test-api/#tests.test_simulation.TestSimulation.test_front_view_is_initialized",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_render_frame_updates_required_information": (
+        "test_render_frame_updates_required_information",
+        "../test-api/#tests.test_simulation.TestSimulation.test_render_frame_updates_required_information",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_calibration_map_displays_all_points_without_legend": (
+        "test_calibration_map_displays_all_points_without_legend",
+        "../test-api/#tests.test_simulation.TestSimulation.test_calibration_map_displays_all_points_without_legend",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_current_calibration_point_color_tracks_rendered_point": (
+        "test_current_calibration_point_color_tracks_rendered_point",
+        "../test-api/#tests.test_simulation.TestSimulation.test_current_calibration_point_color_tracks_rendered_point",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_pause_stops_animation_and_keeps_current_point": (
+        "test_pause_stops_animation_and_keeps_current_point",
+        "../test-api/#tests.test_simulation.TestSimulation.test_pause_stops_animation_and_keeps_current_point",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_resume_restarts_from_current_point": (
+        "test_resume_restarts_from_current_point",
+        "../test-api/#tests.test_simulation.TestSimulation.test_resume_restarts_from_current_point",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_seek_while_paused_selects_point": (
+        "test_seek_while_paused_selects_point",
+        "../test-api/#tests.test_simulation.TestSimulation.test_seek_while_paused_selects_point",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_seek_while_playing_pauses_automatically": (
+        "test_seek_while_playing_pauses_automatically",
+        "../test-api/#tests.test_simulation.TestSimulation.test_seek_while_playing_pauses_automatically",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_seek_renders_selected_point_immediately": (
+        "test_seek_renders_selected_point_immediately",
+        "../test-api/#tests.test_simulation.TestSimulation.test_seek_renders_selected_point_immediately",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_animation_completion_stays_at_last_point": (
+        "test_animation_completion_stays_at_last_point",
+        "../test-api/#tests.test_simulation.TestSimulation.test_animation_completion_stays_at_last_point",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_resume_after_completion_restarts_at_first_point": (
+        "test_resume_after_completion_restarts_at_first_point",
+        "../test-api/#tests.test_simulation.TestSimulation.test_resume_after_completion_restarts_at_first_point",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_view_has_point_based_seek_bar_with_large_handle": (
+        "test_view_has_point_based_seek_bar_with_large_handle",
+        "../test-api/#tests.test_simulation.TestSimulation.test_view_has_point_based_seek_bar_with_large_handle",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_playback_button_label_follows_state": (
+        "test_playback_button_label_follows_state",
+        "../test-api/#tests.test_simulation.TestSimulation.test_playback_button_label_follows_state",
+    ),
+    "TESTCODE:tests.test_simulation.TestSimulation.test_progress_text_uses_point_count_not_time": (
+        "test_progress_text_uses_point_count_not_time",
+        "../test-api/#tests.test_simulation.TestSimulation.test_progress_text_uses_point_count_not_time",
+    ),
 }
 
 _MARKER = re.compile(r"\[\[(" + "|".join(map(re.escape, _TRACE_TARGETS)) + r")\]\]")
